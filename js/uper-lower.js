9d39d3ab063285a3c59cb01ce0913f148ce52225
Subodh Aryal <43497160+asubodh@users.noreply.github.com>
2021-05-25 19:07:36 +0545

    Added Upper and Lower Case!!
    Added Upper and Lower Case!!

---
+++ js/uper-lower.js
@@ -0,0 +1,24 @@
+//UPPERCASE LOWER CASE
+
+function lowerupper() {
+    var str = document.getElementById('upper').value
+    document.getElementById('upper').value = str.toUpperCase();
+    var s = document.getElementById('lower').value
+    document.getElementById('lower').value = s.toLowerCase();
+}
+
+//COPY TEXT
+
+function myFunction() {
+    var copyText = document.getElementById("upper");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
+
+function my_Function() {
+    var copyText = document.getElementById("lower");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
